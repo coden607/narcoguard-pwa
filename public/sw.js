0e9111339871c15af737bce80bde4ebb5a513e57
Steve <coden607@gmail.com>
2026-07-23 06:26:28 +0000

    Make service worker installation resilient

--- public/sw.js
+++ public/sw.js
@@ -11,7 +11,7 @@ const APP_SHELL = [
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(APP_SHELL))
+      .then((cache) => Promise.all(APP_SHELL.map((asset) => cache.add(asset).catch(() => undefined)) ))
   );
   self.skipWaiting();
 });
